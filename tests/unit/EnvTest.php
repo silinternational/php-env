edec73ec6514ecefd4789d992b11b1e78989c40d
Matt Henderson <matt_henderson@sil.org>
2016-04-27 10:29:30 -0400

    Add/update tests to ensure case of other values is unchanged.

--- tests/unit/EnvTest.php
+++ tests/unit/EnvTest.php
@@ -83,12 +83,40 @@ class EnvTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($expected, $actual);
     }
     
-    public function testGet_nonEmptyString()
+    public function testGet_nonEmptyLowercaseString()
     {
         // Arrange
-        $name = 'TEST_NON_EMPTY_STRING';
+        $name = 'TEST_NON_EMPTY_LOWERCASE_STRING';
         $this->putEnv($name . '=abc123');
         $expected = 'abc123';
+        
+        // Act
+        $actual = Env::get($name);
+
+        // Assert
+        $this->assertSame($expected, $actual);
+    }
+    
+    public function testGet_nonEmptyMixedCaseString()
+    {
+        // Arrange
+        $name = 'TEST_NON_EMPTY_MIXED_CASE_STRING';
+        $this->putEnv($name . '=aBc123');
+        $expected = 'aBc123';
+        
+        // Act
+        $actual = Env::get($name);
+
+        // Assert
+        $this->assertSame($expected, $actual);
+    }
+    
+    public function testGet_nonEmptyUppercaseString()
+    {
+        // Arrange
+        $name = 'TEST_NON_EMPTY_UPPERCASE_STRING';
+        $this->putEnv($name . '=ABC123');
+        $expected = 'ABC123';
         
         // Act
         $actual = Env::get($name);
